64bc7d690f3d64910c351405f2153323ee695273
mokeefe09 <michael.okeefe123@gmail.com>
2021-08-27 15:19:37 -0500

    Added TODO list to working directory. Changed indentation style of source files

--- main.c
+++ main.c
@@ -3,42 +3,38 @@
 #include "TM4C123GH6PM.h"
 #include <stdint.h>
 
-#define THRESHOLD 9100
+#define THRESHOLD 9100.
 
 int main(void){
-  uint16_t adc_result;
-  float thermistor;
-  uint16_t other_therm;
+	uint16_t adc_result;
+	float thermistor;
   
-  adc_init();
-  pwm_init();
+	// TODO: ONLY DEBUGGING
+	int count;
+  
+	adc_init();
+	pwm_init();
   
   
-  while(1){
-    // Wait until fifo has a result
-    while (adc_fifo_empty());
+	while(1){
+		// Wait until fifo has a result
+		while (adc_fifo_empty());
     
-    // Read the ADC result
-    adc_result = adc_fifo_read();
+		// Read the ADC result
+		adc_result = adc_fifo_read();
     
-    // Calculate resistance of the thermistor.
-    thermistor = 10000.0 / ((4095.0 / (float)adc_result) - 1.0);
-    other_therm = (uint16_t) thermistor;
-    
-    if (other_therm < THRESHOLD){
-      // Enable the PWM signal
-      PWM0->ENABLE |= (1U << 0);
+		// Calculate resistance of the thermistor.
+		thermistor = 10000.0 / ((4095.0 / (float)adc_result) - 1.0);
+ 
+		if (thermistor < THRESHOLD){
+			// Enable the PWM signal
+			PWM0->ENABLE |= (1U << 0);
 
-    }
-    else{
-      // Disable the PWM signal
-      PWM0->ENABLE &= ~(1U << 0);
-    }
-    
-    
-    
-    
-  }
-  
+		}
+		else{
+			// Disable the PWM signal
+			PWM0->ENABLE &= ~(1U << 0);
+		}
+	}
 }
 
